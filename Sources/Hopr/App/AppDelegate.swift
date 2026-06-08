1cd5fa73a71cff6caf575af1f3c2a2b9b065877a
Zero Dev <akbarabdul80@gmail.com>
2026-06-08 09:48:23 +0700

    feat: use custom Hollow Knight SVG icon in menu bar

--- Sources/Hopr/App/AppDelegate.swift
+++ Sources/Hopr/App/AppDelegate.swift
@@ -125,8 +125,8 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 
     private func loadMenubarImage() -> NSImage? {
         let fm = FileManager.default
-        let localPath = fm.currentDirectoryPath + "/icon.png"
-        let absolutePath = "/Users/macbook/Documents/Project/clone_hopr/icon.png"
+        let localPath = fm.currentDirectoryPath + "/icon.svg"
+        let absolutePath = "/Users/macbook/Documents/Project/clone_hopr/icon.svg"
         
         var path: String? = nil
         if fm.fileExists(atPath: localPath) {
